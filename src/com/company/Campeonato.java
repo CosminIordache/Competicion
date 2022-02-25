cd5e0386d266907942de794b71a8ca48d75ba539
CosminIordache <cosminionutiordache@gmail.com>
2022-02-25 17:44:53 +0100

    commit campeonato

--- src/com/company/Campeonato.java
+++ src/com/company/Campeonato.java
@@ -1,6 +1,74 @@
 package com.company;
 
-public class Campeonato {
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
 
+public class Campeonato {
+    private LocalDate start;
+    private LocalDate finish;
+    private Prueba prueba;
+    private List<Atleta> typeAtlete = new LinkedList<>();
+    private List<Marca> typeMarca = new LinkedList<>();
+
+    public Campeonato(LocalDate start, LocalDate finish, Prueba prueba){
+        this.start = start;
+        this.finish = finish;
+        this.prueba = prueba;
+    }
+
+    public boolean inscribeAtleta(Atleta atleta){
+
+        if (atleta.isActive(start) && !typeAtlete.contains(atleta)){
+            typeAtlete.add(atleta);
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean marcaRealiazda(Marca marca){
+
+        if (prueba == marca.getPrueba()){
+            typeMarca.add(marca);
+            return true;
+        }
+
+
+        for (Atleta atleta : typeAtlete){
+             if (!atleta.debut(start)){
+                 return false;
+             }
+        }
+        if (start == null){
+             return false;
+        }
+        return false;
+    }
+    public boolean iniciar(){
+        if (start == null){return true;};
+
+
+        if (finish == null){
+            return true;
+        }
+
+        return false;
+    }
+    public boolean finalizar(){
+        if (finish == null){return true;}
+        if (start != null){return true;}
+        return false;
+    }
+
+    public Marca mejorMarca (){
+        Marca mejorMarca = typeMarca.get(0);
+        for (Marca marca : typeMarca){
+            if (marca.getResultado() < mejorMarca.getResultado()){
+                mejorMarca = marca;
+            }
+        }
+        return mejorMarca;
+    }
 
 }
--- src/com/company/Marca.java
+++ src/com/company/Marca.java
@@ -3,8 +3,22 @@ package com.company;
 public class Marca {
 
     private Prueba prueba;
+
+    public Prueba getPrueba() {
+        return prueba;
+    }
+
     private Atleta deportista;
+
+    public Atleta getDeportista() {
+        return deportista;
+    }
+
     private int resultado;
+
+    public int getResultado() {
+        return resultado;
+    }
 
     public Marca (Prueba prueba, Atleta deportista, int resultado){
         this.prueba = prueba;
